a33afe51a476bd7347a3ba14cc4cb7be74f7710b
Prince Jaiswal <prince.jaiswal@avantika.edu.in>
2026-02-16 16:52:22 +0530

    Insertion sort algorithm added

--- insertion sort.cpp
+++ insertion sort.cpp
@@ -1,6 +1,30 @@
 #include<stdio.h>
+void printarray(int* arr,int n)
+{   
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+}
+void insertionsort(int* arr,int start,int end)
+{
+    for(int i=start+1;i<end;i++)
+    {
+        int cur=arr[i];
+        int prev=i-1;
+        while(arr[prev]>cur)
+        {
+            arr[prev+1]=arr[prev];
+            prev--;
+        }
+        arr[prev+1]=cur;      
+    }
+}
 int main()
 {
-    
+    int array[10]={12,34,2,4,434,223,53,23,234,34};
+    int n=sizeof(array)/sizeof(array[0]);
+    insertionsort(array,0,n);
+    printarray(array,n);
     return 0;
 }
